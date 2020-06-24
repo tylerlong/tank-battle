1b81ac95e8eb6e221c665cca2cb4fdc220a14df4
Tyler Long <tyler4long@gmail.com>
2020-06-24 10:29:09 -0700

    Convert to TS

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+/* eslint-disable node/no-unpublished-import */
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts'], // so that you can `import './main'` instead of `import './main.ts'`.
+  },
+};
+
+export default config;
