9c325e8f732a8f6d18d7dece29fb7b1d1f02febe
Tyler Long <tyler4long@gmail.com>
2020-06-23 22:01:14 -0700

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
+}
+
+module.exports = config;
