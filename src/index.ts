1bebf7d6c9f8ab8ee7c63f2cdfd291e5f521fdea
Tyler Long <tyler4long@gmail.com>
2020-06-26 21:28:48 -0700

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import * as Phaser from 'phaser';
 
 import MainScene from './main-scene';
 
-const gameConfig = {
+new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   parent: 'game-container',
-  pixelArt: true,
+  render: {
+    pixelArt: true,
+  },
   physics: {
     default: 'arcade',
     arcade: {
@@ -17,6 +19,4 @@ const gameConfig = {
     },
   },
   scene: [MainScene],
-};
-
-new Phaser.Game(gameConfig);
+});
