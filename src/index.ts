587152c48254c6e99106bb410a2183531f114e30
Tyler Long <tyler4long@gmail.com>
2020-06-26 21:51:01 -0700

    Game takes full window

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as Phaser from 'phaser';
 
 import MainScene from './main-scene';
 
-new Phaser.Game({
+const game = new Phaser.Game({
   type: Phaser.AUTO,
   width: 800,
   height: 600,
@@ -20,3 +20,17 @@ new Phaser.Game({
   },
   scene: [MainScene],
 });
+
+// game.scale.toggleFullscreen();
+// setTimeout(() => game.scale.toggleFullscreen(), 3000);
+
+setTimeout(() => resizeGame(), 3000);
+window.addEventListener('resize', resizeGame);
+function resizeGame() {
+  console.log('resize game');
+  const canvas = document.querySelector('canvas');
+  if (canvas !== null) {
+    // game is ready
+    game.scale.setGameSize(window.innerWidth, window.innerHeight);
+  }
+}
